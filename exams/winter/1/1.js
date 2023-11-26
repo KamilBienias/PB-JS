fff08d96e65f6780e49e7d45f651330865cb9b02
KamilBienias <kamil.bienias.math@gmail.com>
2023-11-26 11:58:55 +0100

    compare weights function and test

--- exams/winter/1/1.js
+++ exams/winter/1/1.js
@@ -21,3 +21,25 @@ console.log("groupA", groupA);
 console.log("groupB", groupB);
 console.log("groupC", groupC);
 
+function compareWeights(group1, group2) {
+    const weightGroup1 = group1.reduce((acc, val) => acc + val, 0);
+    const weightGroup2 = group2.reduce((acc, val) => acc + val, 0);
+  
+    if (weightGroup1 === weightGroup2) {
+      return 0;
+    } else if (weightGroup1 > weightGroup2) {
+      return 1;
+    } else {
+      return -1;
+    }
+}
+
+console.log("Test: compare groupA with groupB.");
+const testComparison = compareWeights(groupA, groupB);
+if (testComparison == 0){
+    console.log("groups have the same weight");
+} else if (testComparison == 1){
+    console.log("groupA is heavier");
+} else if (testComparison == -1){
+    console.log("groupB is heavier");
+}
