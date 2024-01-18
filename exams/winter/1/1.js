411392363b46cbf2d2d93183a43b4345a59a37f7
KamilBienias <kamil.bienias.math@gmail.com>
2024-01-18 22:41:47 +0100

    two comparisons

--- exams/winter/1/1.js
+++ exams/winter/1/1.js
@@ -12,7 +12,7 @@ function insertHeavyBall(arr) {
 }
   
 insertHeavyBall(balls);
-console.log(balls);
+console.log("balls", balls);
 
 const groupA = balls.slice(0, 3);
 const groupB = balls.slice(3, 6);
@@ -35,11 +35,36 @@ function compareWeights(group1, group2) {
 }
 
 console.log("Test: compare groupA with groupB.");
-const testComparison = compareWeights(groupA, groupB);
-if (testComparison == 0){
-    console.log("groups have the same weight");
-} else if (testComparison == 1){
-    console.log("groupA is heavier");
-} else if (testComparison == -1){
-    console.log("groupB is heavier");
+const firstComparison = compareWeights(groupA, groupB);
+if (firstComparison == 0){
+    console.log("groupA and groupB have the same weight");
+    console.log("Test: compare [groupC[0]] with [groupC[1]].");
+    const secondComparison = compareWeights([groupC[0]], [groupC[1]]);
+    if (secondComparison == 1) {
+      console.log("Ball 6 is heavier than ball 7. The heavy ball is at index 6");
+    } else if (secondComparison == -1) {
+      console.log("Ball 7 is heavier than ball 6. The heavy ball is at index 7");
+    }
+} else if (firstComparison == 1){
+    console.log("groupA is heavier than groupB");
+    console.log("Test: compare [groupA[0]] with [groupA[1]].");
+    const secondComparison = compareWeights([groupA[0]], [groupA[1]]);
+    if (secondComparison === 0) {
+        console.log("Ball 0 and ball 1 have the same weight. The heavy ball is at index 2.");
+    } else if (secondComparison === 1) {
+        console.log("Ball 0 is heavier than ball 1. The heavy ball is at index 0.");
+    } else {
+        console.log("Ball 1 is heavier than ball 0. The heavy ball is at index 1.");
+    }
+} else if (firstComparison == -1){
+    console.log("groupB is heavier than groupA");
+    console.log("Test: compare [groupB[0]] with [groupB[1]].");
+    const secondComparison = compareWeights([groupB[0]], [groupB[1]]);
+    if (secondComparison === 0) {
+        console.log("Ball 3 and ball 4 have the same weight. The heavy ball is at index 5.");
+    } else if (secondComparison === 1) {
+        console.log("Ball 3 is heavier than ball 4. The heavy ball is at index 3.");
+    } else {
+        console.log("Ball 4 is heavier than ball 3. The heavy ball is at index 4.");
+    }
 }
