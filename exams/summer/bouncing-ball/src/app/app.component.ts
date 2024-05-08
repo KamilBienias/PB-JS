a34b6f58e3776ffff5a9dd80f61105cbff50ec7d
KamilBienias <kamil.bienias.math@gmail.com>
2024-05-08 16:52:41 +0200

    done with fixed ballPosition and ballDirection

--- exams/summer/bouncing-ball/src/app/app.component.ts
+++ exams/summer/bouncing-ball/src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
     ['X', '0', '0', '0', '0', '0', '0', '0', 'X', 'X', 'X', 'X'],
     ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
     ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
-    ['X', '0', '0', '0', 'X', '0', '0', '0', '0', 'Y', '0', 'X'],
+    ['X', '0', '0', '0', 'X', '0', '0', '0', '0', 'Y', '0', 'X'],// trzeci od prawej był Y
     ['X', '0', '0', 'X', 'X', 'X', '0', '0', '0', '0', '0', 'X'],
     ['X', '0', '0', '0', 'X', '0', '0', '0', '0', '0', '0', 'X'],
     ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
@@ -44,29 +44,25 @@ export class AppComponent implements OnInit {
     const moveBall = () => {
       const nextX = this.ballPosition.x + ballDirection.dx;
       const nextY = this.ballPosition.y + ballDirection.dy;
+      const theSameX = this.ballPosition.x;
+      const theSameY = this.ballPosition.y;
   
-      console.log('Next X:', nextX, 'Next Y:', nextY);
-  
-      
-      if (this.board[nextY][nextX] === 'X') {
+      // Kolizja z obiektem Y
+      if (this.board[nextY][nextX] === 'Y') { 
+        // Zmiana kierunku na losowy
+        ballDirection = this.generateRandomDirection(ballDirection);
 
-          // odbicie od poziomej ściany
-          if (
-          (nextX - 1 >= 0 && this.board[nextY][nextX-1] === 'X') ||
-          (nextX + 1 < this.board[0].length && this.board[nextY][nextX+1] === 'X')
-          ) { 
-            ballDirection.dy *= -1;
-          }
-
-          // odbicie od pionowej ściany
-          if (
-            (nextY - 1 >= 0 && this.board[nextY-1][nextX] === 'X') ||
-            (nextY + 1 < this.board.length && this.board[nextY+1][nextX] === 'X')
-          ) { 
-            ballDirection.dx *= -1;
-          }
-
-
+        // Zmiana obiektu Y na puste pole
+        this.board[nextY][nextX] = '0';
+      }
+      // odbicie od spiczastego kantu
+      else if (
+        this.board[nextY][nextX] === 'X' 
+        && this.board[theSameY][nextX] === '0'
+        && this.board[nextY][theSameX] === '0'
+      ) {
+        ballDirection.dx *= -1;
+        ballDirection.dy *= -1;
 
         // Aktualizacja planszy
         this.board[this.ballPosition.y][this.ballPosition.x] = '0'; // Usuń piłkę z aktualnej pozycji
@@ -74,18 +70,55 @@ export class AppComponent implements OnInit {
         this.ballPosition.x += ballDirection.dx;
         this.ballPosition.y += ballDirection.dy;
         this.board[this.ballPosition.y][this.ballPosition.x] = '1'; // Oznacz aktualną pozycję piłki
-        
-        console.log('Ball direction po zmianie:', ballDirection);
-      } else if (this.board[nextY][nextX] === 'Y') { // Kolizja z obiektem Y
-        console.log('Kolizja z obiektem Y. Ball direction przed zmianą:', ballDirection);
-        // Zmiana kierunku na losowy
-        ballDirection = this.generateRandomDirection(ballDirection);
+      } 
+      // odbicie od wklęsłego kąta tak jak w pokoju
+      else if (
+        // this.board[nextY][nextX] === 'X' &&
+        this.board[theSameY][nextX] === 'X' &&
+        this.board[nextY][theSameX] === 'X'
+      ) {
+        ballDirection.dx *= -1;
+        ballDirection.dy *= -1;
 
-        // Zmiana obiektu Y na puste pole
-        this.board[nextY][nextX] = '0';
-        
-        console.log('Ball direction po zmianie:', ballDirection);
-      } else {
+        // Aktualizacja planszy
+        this.board[this.ballPosition.y][this.ballPosition.x] = '0'; // Usuń piłkę z aktualnej pozycji
+        this.previousBallPosition = this.ballPosition; // Zapamiętaj poprzednią pozycję piłki
+        this.ballPosition.x += ballDirection.dx;
+        this.ballPosition.y += ballDirection.dy;
+        this.board[this.ballPosition.y][this.ballPosition.x] = '1'; // Oznacz aktualną pozycję piłki
+      }
+      // odbicie od pionowej ściany
+      else if (
+        this.board[theSameY][nextX] === 'X'
+        && this.board[nextY][theSameX] === '0'
+      ) {
+        ballDirection.dx *= -1;
+        ballDirection.dy *= 1;
+
+        // Aktualizacja planszy
+        this.board[this.ballPosition.y][this.ballPosition.x] = '0'; // Usuń piłkę z aktualnej pozycji
+        this.previousBallPosition = this.ballPosition; // Zapamiętaj poprzednią pozycję piłki
+        this.ballPosition.x += ballDirection.dx;
+        this.ballPosition.y += ballDirection.dy;
+        this.board[this.ballPosition.y][this.ballPosition.x] = '1'; // Oznacz aktualną pozycję piłki
+      }
+      // odbicie od poziomej ściany
+      else if (
+        this.board[theSameY][nextX] === '0'
+        && this.board[nextY][theSameX] === 'X'
+      ) {
+        ballDirection.dx *= 1;
+        ballDirection.dy *= -1;
+
+        // Aktualizacja planszy
+        this.board[this.ballPosition.y][this.ballPosition.x] = '0'; // Usuń piłkę z aktualnej pozycji
+        this.previousBallPosition = this.ballPosition; // Zapamiętaj poprzednią pozycję piłki
+        this.ballPosition.x += ballDirection.dx;
+        this.ballPosition.y += ballDirection.dy;
+        this.board[this.ballPosition.y][this.ballPosition.x] = '1'; // Oznacz aktualną pozycję piłki
+      }
+      // gdy nie napotka na X ani Y 
+      else {
         // Aktualizacja planszy
         this.board[this.ballPosition.y][this.ballPosition.x] = '0'; // Usuń piłkę z aktualnej pozycji
         this.previousBallPosition = this.ballPosition; // Zapamiętaj poprzednią pozycję piłki
@@ -103,13 +136,15 @@ export class AppComponent implements OnInit {
 
   // Funkcja generująca losowy kierunek inny niż aktualny kierunek piłki
   generateRandomDirection(currentDirection: { dx: number; dy: number }): { dx: number; dy: number } {
-    let dx: number, dy: number;
-  
-    do {
+    let dx = Math.random() < 0.5 ? -1 : 1;
+    let dy = Math.random() < 0.5 ? -1 : 1;
+
+    // Sprawdzamy czy nowy kierunek jest różny od aktualnego
+    while (dx === currentDirection.dx && dy === currentDirection.dy) {
       dx = Math.random() < 0.5 ? -1 : 1;
       dy = Math.random() < 0.5 ? -1 : 1;
-    } while (dx === currentDirection.dx && dy === currentDirection.dy);
-  
+    }
+
     return { dx, dy };
   }
   
